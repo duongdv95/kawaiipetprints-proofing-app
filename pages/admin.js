03e8f84fcc04c81b3d683273d1e19c4dd793e82b
Daniel <danielvanduong@gmail.com>
2020-01-10 01:26:48 -0800

    prelim order table work

--- pages/admin.js
+++ pages/admin.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react'
+import Modal from 'react-modal';
 import Header from '../components/header.js'
 import axios from 'axios'
+Modal.setAppElement("#__next")
 
 const meta = { title: 'Order Dashboard', description: 'Order Dashboard' }
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class Admin extends React.Component {
   constructor (props) {
@@ -11,9 +24,13 @@ class Admin extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
     this.state = {
       loading: true,
-      orderData: {}
+      orderData: {},
+      modalIsOpen: false
     }
-    
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
     // this.fetchData = this.fetchData.bind(this)
   }
   async componentDidMount () {
@@ -24,7 +41,18 @@ class Admin extends React.Component {
     const { data } = await axios.get(`/admin/api/getorders`)
     this.setState({orderData: data.orders})
   }
-
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+ 
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+ 
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
   handleChange(event) {
     event.preventDefault()
     const eventType = event.target.name
@@ -58,26 +86,51 @@ class Admin extends React.Component {
         </Header>
         <div style={{ padding: "0 1em"}}>
         <img src="/static/logo.png" alt='' width="200px" />
-        <div style={{display:"grid", gridTemplateColumns:"1fr 1fr 1fr"}}>
-            <div>
-                Order Number
+        <div id="order-table" >
+            <div className="item header">
+              Proof Status
             </div>
-            <div>
-                Email
+            <div className="item header">
+              Upload Art
             </div>
-            <div>
-                Proof Status
+            <div className="item header">
+              Order Number
             </div>
-            <div>
-                1001
+            <div className="item header">
+              Date
             </div>
-            <div>
-                danielvanduong@gmail.com
+            <div className="item" style={{backgroundColor: "red", color: "white"}}>
+              Awaiting Art Upload
             </div>
-            <div>
-                Awaiting Art Upload
+            <div className="item">
+              <button onClick={this.openModal}>Upload</button>
+            </div>
+            <div className="item">
+              1001
+            </div>
+            <div className="item">
+              1/10/20
             </div>
         </div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
         </div>
       </div>
     )
