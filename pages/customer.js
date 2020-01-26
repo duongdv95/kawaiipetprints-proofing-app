236f264d5feba4bdc6dfea16df77628e90244cc9
Daniel <danielvanduong@gmail.com>
2020-01-25 22:14:05 -0800

    semi completed carousel / just need api endpoints

--- pages/customer.js
+++ pages/customer.js
@@ -1,44 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Header from '../components/header.js'
-import Slider from "react-slick"
+// import Slider from "react-slick"
 import axios from 'axios'
 import Modal from 'react-modal';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup, WithStore, Dot } from 'pure-react-carousel';
+
 Modal.setAppElement("#__next")
 var moment = require('moment');
 const meta = { title: 'Order Dashboard', description: 'Order Dashboard' }
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: '40px',
+    bottom: 'unset',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
 };
-
-function SimpleSlider(props) {
-  const { backgroundsArray } = props
-  const settings = {
-    dots: true,
-    speed: 200 //milliseconds
-  }
-  const carousel = backgroundsArray.map(function(element, index) {
-    return (
-      <div key={index}>
-        <img src={element} ></img>
-      </div>
-    )
-  })
-
-  return (
-    <div className="container">
-      <Slider {...settings}>
-        {carousel}
-      </Slider>
-    </div>
-  )
-}
 
 function History() {
   return (
@@ -63,14 +42,18 @@ function History() {
 }
 
 function OrderProof(props) {
-  const { backgroundCategories, currentBackground, backgroundsArray, orderInfo, loading, openModal } = props
-  const orderMap = (!loading && orderInfo.items.proof_created) ? orderInfo.items.line_items.map(function(element, index){
+  const {
+    orderInfo, loading, openModal, selectedBackgroundArray } = props
+  const orderMap = (!loading && orderInfo.items.proof_created) ? orderInfo.items.line_items.map(function (element, index) {
     return (
       <div key={index}>
         <h3>{element.product_name}</h3>
-        <button>Request Revision</button><button onClick={() => openModal({ index, order_id: orderInfo.items.order_id})}>Select Background</button>
+        <button>Request Revision</button>
+        <button onClick={() => {
+          openModal({ currentLineItem: index })
+        }}>Select Background</button>
         <div className="wrapper">
-          <img src="/static/white.png" className="bg-image"></img>
+          <img src={selectedBackgroundArray[index]} className="bg-image"></img>
           <div className="other-images">
             <img src={element.artworkURL} />
           </div>
@@ -86,7 +69,7 @@ function OrderProof(props) {
   }) : (null)
 
   const orderStatus = (!loading) ? (<a href={orderInfo.items.line_items}>Order Status</a>) : (null)
-  return(
+  return (
     <div>
       <h1>Your Order Proof</h1>
       {orderStatus}
@@ -97,40 +80,169 @@ function OrderProof(props) {
   )
 }
 
+class ChangeCategory extends React.Component {
+  render() {
+    const { handleSubmit, updateCurrentSlide } = this.props
+    return (
+      <div>
+        <button onClick={(event) => {
+          this.props.carouselStore.setStoreState({currentSlide: 0})
+          updateCurrentSlide(0)
+          handleSubmit(event)
+        }} name="category-prev" type="submit">Prev</button>
+        <button onClick={(event) => {
+          this.props.carouselStore.setStoreState({currentSlide: 0})
+          updateCurrentSlide(0)
+          handleSubmit(event)
+        }} name="category-next" type="submit">Next</button>
+      </div>
+    )
+  }
+}
+
+class BackButton extends React.Component {
+  render() {
+    const { currentSlide, totalSlides, updateCurrentSlide }  = this.props
+    const updatedSlide = (currentSlide === 0) ? totalSlides - 1 : currentSlide - 1 
+    console.log(updatedSlide)
+    return (
+      <ButtonBack onClick={()=>updateCurrentSlide(updatedSlide)}>Back</ButtonBack>
+    )
+  }
+}
+
+class NextButton extends React.Component {
+  render() {
+    const { currentSlide, totalSlides, updateCurrentSlide } = this.props
+    const updatedSlide = (currentSlide ===  totalSlides - 1) ? 0 : currentSlide + 1 
+    return (
+      <ButtonNext onClick={()=>updateCurrentSlide(updatedSlide)}>Next</ButtonNext>
+    )
+  }
+}
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlide: 0
+    }
+  }
+  render() {
+    const style = {width: "100%"}
+    const { 
+      backgroundCategories, 
+      currentCategory,
+      currentLineItem,
+      handleSubmit,
+      backgroundsArray,
+      updateCurrentSlide
+    } = this.props
+
+    const backgroundsMap = backgroundsArray[currentCategory].map((element, index)=>{
+      return (
+        <Slide key={index} index={index}><img style={style} src={element} /></Slide>
+      )
+    })
+
+    const CarouselChangeCategory = WithStore(ChangeCategory, (state) => ({
+      currentSlide: state.currentSlide, 
+      handleSubmit,
+      updateCurrentSlide 
+    }))
+
+    const EnhancedBackButton = WithStore(BackButton, (state) => ({
+      currentSlide: state.currentSlide,
+      updateCurrentSlide,
+      totalSlides: backgroundsMap.length
+    }))
+    const EnhancedNextButton = WithStore(NextButton, (state) => ({
+      currentSlide: state.currentSlide,
+      updateCurrentSlide,
+      totalSlides: backgroundsMap.length
+    }))
+
+    const dotMap = backgroundsArray[currentCategory].map((element, index) => {
+      return (
+        <Dot 
+        key={index}
+        slide={index}
+        onClick={()=>{updateCurrentSlide(index)}}
+        />
+      )
+    })
+    return (
+      <div>
+        <span style={{ fontWeight: "bold" }}>{backgroundCategories[currentCategory]}</span>
+        <CarouselProvider
+          naturalSlideWidth={120}
+          naturalSlideHeight={100}
+          totalSlides={backgroundsMap.length}
+          infinite={true}
+        >
+          <CarouselChangeCategory/>
+          <Slider>
+            {backgroundsMap}
+          </Slider>
+          <div>
+            {dotMap}
+          </div>
+          {/* <DotGroup/> */}
+          <EnhancedBackButton></EnhancedBackButton>
+          <EnhancedNextButton></EnhancedNextButton>
+        </CarouselProvider>
+        <button
+          data-currentlineitem={currentLineItem}
+          onClick={handleSubmit}
+          name="select-bg"
+          type="submit"
+        >
+          Select Background
+        </button>
+      </div> 
+    )
+  }
+}
+
 class Customer extends React.Component {
-  static getInitialProps ({ query }) {
-    let props = {order_id: query.order_id}
+  static getInitialProps({ query }) {
+    let props = { order_id: query.order_id }
     return props
   }
-  constructor (props) {
+  constructor(props) {
     super(props)
     // this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-
+    this.slider = React.createRef()
     this.state = {
       loading: true,
-      backgroundCategories: ["Color Pattern", "Plants", "Food", "Animals",  "Pop Culture", "Solid colors"],
-      currentBackground: 0,
-      backgroundsArray: 
-      [
-        ["/static/pattern1.png", "/static/pattern2.png"], 
-        ["/static/floral1.png", "/static/floral2.png", "/static/floral3.png"],
-        ["/static/food1.png", "/static/food2.png"],
-        ["/static/food2.png"],
-        ["/static/food2.png"],
-        ["/static/food2.png"]
-      ],
+      backgroundCategories: ["Color Pattern", "Plants", "Food", "Animals", "Pop Culture", "Solid colors"],
+      currentCategory: 0,
+      backgroundsArray:
+        [
+          ["/static/pattern1.png", "/static/pattern2.png"],
+          ["/static/floral1.png", "/static/floral2.png", "/static/floral3.png"],
+          ["/static/food1.png", "/static/food2.png"],
+          ["/static/food2.png"],
+          ["/static/food2.png"],
+          ["/static/food2.png"]
+        ],
+      currentSlide: 0,
       orderInfo: {},
       totalOrders: 1,
       loading: true,
       message: "",
-      modalIsOpen: false
+      modalIsOpen: false,
+      currentLineItem: 0,
+      selectedBackgroundArray: [],
+      slideIndex: 0,
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
     // this.fetchData = this.fetchData.bind(this)
   }
-  async componentDidMount () {
+  
+  async componentDidMount() {
     await this.getOrderInfo()
   }
 
@@ -139,25 +251,30 @@ class Customer extends React.Component {
       const { data } = await axios.get(
         `/api/getorder?order_id=${this.props.order_id}`
       )
-      if(data.success) {
-        const totalOrders = data.items.line_items.reduce(function(accumulator, currentValue) {
+      if (data.success) {
+        const totalOrders = data.items.line_items.reduce(function (accumulator, currentValue) {
           return accumulator + currentValue.quantity
         }, 0)
+        const selectedBackgroundArray = this.state.selectedBackgroundArray
+        for (let i = 0; i < totalOrders; i++) {
+          selectedBackgroundArray.push("/static/white.png")
+        }
         this.setState({
           orderInfo: data,
           totalOrders,
-          loading: false
+          loading: false,
+          selectedBackgroundArray
         })
       } else {
-        this.setState({message: `Couldn't fetch order ${this.props.order_id}`})
+        this.setState({ message: `Couldn't fetch order ${this.props.order_id}` })
       }
-      
+
     } catch (error) {
-      this.setState({message: `Couldn't fetch order ${this.props.order_id}`})
+      this.setState({ message: `Couldn't fetch order ${this.props.order_id}` })
     }
   }
 
-  renderDogList () {
+  renderDogList() {
     return (
       <ul>
         {this.state.dogs.map((dog, key) =>
@@ -174,62 +291,81 @@ class Customer extends React.Component {
     const eventType = event.target.name
     switch (eventType) {
       case "category-prev":
-        (this.state.currentBackground === 0) ? this.setState({ currentBackground: 5}) : this.setState({ currentBackground: this.state.currentBackground - 1})
-        break
+        (this.state.currentCategory === 0) ?
+        this.setState({ currentCategory: 5 })
+          :
+          this.setState({ currentCategory: this.state.currentCategory - 1 })
+          break
 
       case "category-next":
-        (this.state.currentBackground === 5) ? this.setState({ currentBackground: 0}) : this.setState({ currentBackground: this.state.currentBackground + 1})
+        (this.state.currentCategory === 5) ?
+          this.setState({ currentCategory: 0 })
+          :
+          this.setState({ currentCategory: this.state.currentCategory + 1 })
+        break
+      case "select-bg":
+        const currentLineItem = event.target.dataset.currentlineitem
+        const { selectedBackgroundArray, backgroundsArray, currentCategory, currentSlide } = this.state
+        selectedBackgroundArray[currentLineItem] = backgroundsArray[currentCategory][currentSlide]
+        this.closeModal()
+        this.setState({ selectedBackgroundArray })
         break
       default:
         console.log("error")
     }
   }
 
-  openModal({ index, order_id }) {
-    console.log(index, order_id)
-    this.setState({currentOrderID: order_id, modalIsOpen: true});
+  openModal({ currentLineItem }) {
+    this.setState({ currentLineItem, modalIsOpen: true });
   }
 
   closeModal() {
     this.setState({ modalIsOpen: false });
   }
-  render () {
+
+  updateCurrentSlide(currentSlide) {
+    console.log(currentSlide)
+    this.setState({ currentSlide })
+  }
+
+  update(current) {
+    console.log(current)
+    this.setState({ slideIndex: current })
+  }
+  render() {
     return (
       <div>
         <Header meta={meta}>
         </Header>
-        <div style={{ padding: "0 1em"}}>
+        <div style={{ padding: "0 1em" }}>
           <a href="/">
             <img src="/static/logo.png" alt='' width="200px" />
           </a>
-          <OrderProof 
-            backgroundCategories={this.state.backgroundCategories} 
-            currentBackground={this.state.currentBackground}
-            backgroundsArray={this.state.backgroundsArray[this.state.currentBackground]}
+          <OrderProof
+            backgroundCategories={this.state.backgroundCategories}
+            currentSlide={this.state.currentSlide}
+            backgroundsArray={this.state.backgroundsArray[this.state.currentCategory]}
             handleSubmit={this.handleSubmit}
             orderInfo={this.state.orderInfo}
             loading={this.state.loading}
             openModal={this.openModal}
+            selectedBackgroundArray={this.state.selectedBackgroundArray}
           />
           <Modal
             isOpen={this.state.modalIsOpen}
             onRequestClose={this.closeModal}
-            // style={customStyles}
+            style={customStyles}
           >
-            <div style={{ fontWeight: "bold", display: "flex", justifyContent: "space-evenly" }}>
-              <button onClick={this.handleSubmit} name="category-prev" type="submit">Prev</button>
-              <span>{this.state.backgroundCategories[this.state.currentBackground]}</span>
-              <button onClick={this.handleSubmit} name="category-next" type="submit">Next</button>
-            </div>
-            <SimpleSlider backgroundsArray={this.state.backgroundsArray[this.state.currentBackground]} />
+            <Carousel
+              backgroundsArray={this.state.backgroundsArray}
+              currentCategory={this.state.currentCategory}
+              backgroundCategories={this.state.backgroundCategories}
+              handleSubmit={this.handleSubmit}
+              currentLineItem={this.state.currentLineItem}
+              updateCurrentSlide={this.updateCurrentSlide.bind(this)}
+            />
           </Modal>
-          {/* <form>
-            <label>Your comments (optional)</label>
-            <textarea style={{ display:"block" }}/>
-            <input name="request-change" type="submit" value="Request Change"/>
-            <input name="approve" type="submit" value="Approve"/>
-          </form> */}
-          <History/>
+          <History />
         </div>
       </div>
     )
